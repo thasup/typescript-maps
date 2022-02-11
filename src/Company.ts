150eb283ab28229567aa04c2976c2c003e5f071d
thasup <thanachonfirst@hotmail.com>
2022-02-12 00:18:29 +0700

    create Company class

---
+++ src/Company.ts
@@ -0,0 +1,18 @@
+import faker from "@faker-js/faker";
+
+export class Company {
+  constructor() {
+    this.companyName = faker.company.companyName();
+    this.catchPhrase = faker.company.catchPhrase();
+    this.location = {
+      lat: parseFloat(faker.address.latitude()),
+      lng: parseFloat(faker.address.longitude()),
+    };
+  }
+  companyName: string;
+  catchPhrase: string;
+  location: {
+    lat: number;
+    lng: number;
+  };
+}
